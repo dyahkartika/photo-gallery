d3e0e1fc84d3efa16eb2bbaa8581512a0a2094ae
Dyah Kartika <dyahkartikad15@gmail.com>
2023-06-26 17:52:42 +0700

    fixing design

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/photos");
+  };
+
   return (
     <>
       <div className="home">
-      <section className="textHome">
-        {/* <SliderMain /> */}
-        <p style={{fontSize: '40px'}}>Keep all your memories here and show others</p>
-      </section>
-        
+        <section className="secHome">
+          <p style={{ fontSize: "40px" }} className="textHome">
+            Keep all your memories here and show others
+          </p>
+          <button className="home-btn" onClick={handleNavigate}>
+          Go to Photos
+        </button>
+        </section>
+        <img src="img/landing.png" alt="home-pic" className="home-pic" />
       </div>
     </>
   );
